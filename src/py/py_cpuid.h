42e1246c713dccd2cad56de39ef414aacd5afb0e
Abhiram V <abhi.raa.man.v@gmail.com>
2025-11-21 21:50:16 +0530

    py: allow setting cpuid_regs.<reg> also

--- src/py/py_cpuid.h
+++ src/py/py_cpuid.h
@@ -26,11 +26,98 @@ static PyObject *Py_cpuid_regs_get_edx(struct Py_cpuid_regs *self, void *closure
 	return PyLong_FromUnsignedLong(self->regs.edx);
 }
 
+static int Py_cpuid_regs_set_eax(struct Py_cpuid_regs *self, PyObject* value, void *closure)
+{
+	if (value == NULL) {
+		PyErr_SetString(PyExc_TypeError, "cannot delete attribute edx");
+		return -1;
+	}
+
+	if (!PyLong_Check(value)) {
+		PyErr_SetString(PyExc_TypeError, "Value must be an integer");
+		return -1;
+	}
+
+	unsigned long v = PyLong_AsUnsignedLong(value);
+	if (v > 0xffffffff) {
+		PyErr_SetString(PyExc_ValueError, "Integer must be a 32-bit unsigned value");
+		return -1;
+	}
+	self->regs.eax = v;
+
+	return 0;
+}
+
+static int Py_cpuid_regs_set_ebx(struct Py_cpuid_regs *self, PyObject* value, void *closure)
+{
+	if (value == NULL) {
+		PyErr_SetString(PyExc_TypeError, "cannot delete attribute edx");
+		return -1;
+	}
+
+	if (!PyLong_Check(value)) {
+		PyErr_SetString(PyExc_TypeError, "Value must be an integer");
+		return -1;
+	}
+
+	unsigned long v = PyLong_AsUnsignedLong(value);
+	if (v > 0xffffffff) {
+		PyErr_SetString(PyExc_ValueError, "Integer must be a 32-bit unsigned value");
+		return -1;
+	}
+	self->regs.ebx = v;
+
+	return 0;
+}
+
+static int Py_cpuid_regs_set_ecx(struct Py_cpuid_regs *self, PyObject* value, void *closure)
+{
+	if (value == NULL) {
+		PyErr_SetString(PyExc_TypeError, "cannot delete attribute edx");
+		return -1;
+	}
+
+	if (!PyLong_Check(value)) {
+		PyErr_SetString(PyExc_TypeError, "Value must be an integer");
+		return -1;
+	}
+
+	unsigned long v = PyLong_AsUnsignedLong(value);
+	if (v > 0xffffffff) {
+		PyErr_SetString(PyExc_ValueError, "Integer must be a 32-bit unsigned value");
+		return -1;
+	}
+	self->regs.ecx = v;
+	return 0;
+}
+
+static int Py_cpuid_regs_set_edx(struct Py_cpuid_regs *self, PyObject* value, void *closure)
+{
+	if (value == NULL) {
+		PyErr_SetString(PyExc_TypeError, "cannot delete attribute edx");
+		return -1;
+	}
+
+	if (!PyLong_Check(value)) {
+		PyErr_SetString(PyExc_TypeError, "Value must be an integer");
+		return -1;
+	}
+
+	unsigned long v = PyLong_AsUnsignedLong(value);
+	if (v > 0xffffffff) {
+		PyErr_SetString(PyExc_ValueError, "Integer must be a 32-bit unsigned value");
+		return -1;
+	}
+	self->regs.edx = v;
+
+	return 0;
+}
+
 static PyGetSetDef cpuid_regs_getset[] = {
-	{"eax", (getter)Py_cpuid_regs_get_eax, NULL, "eax register", NULL},
-	{"ebx", (getter)Py_cpuid_regs_get_ebx, NULL, "ebx register", NULL},
-	{"ecx", (getter)Py_cpuid_regs_get_ecx, NULL, "ecx register", NULL},
-	{"edx", (getter)Py_cpuid_regs_get_edx, NULL, "edx register", NULL},
+	{"eax", (getter)Py_cpuid_regs_get_eax, (setter)Py_cpuid_regs_set_eax, "eax register", NULL},
+	{"ebx", (getter)Py_cpuid_regs_get_ebx, (setter)Py_cpuid_regs_set_ebx, "ebx register", NULL},
+	{"ecx", (getter)Py_cpuid_regs_get_ecx, (setter)Py_cpuid_regs_set_ecx, "ecx register", NULL},
+	{"edx", (getter)Py_cpuid_regs_get_edx, (setter)Py_cpuid_regs_set_edx, "edx register", NULL},
 	{NULL}
 };
 
